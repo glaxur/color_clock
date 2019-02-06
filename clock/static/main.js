098efa0d1f3089811a8aba165a72fbcfe2c23ad2
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-02-06 16:17:07 -0500

    added hover

--- clock/static/main.js
+++ clock/static/main.js
@@ -1,5 +1,17 @@
-console.log('hey');
+console.log('hello');
 var d, hour, minute, second, color;
+
+var isHovering = false;
+
+$("#timeid").mouseenter(function(event){
+    isHovering = true;
+    console.log('I\'m hovering');
+});
+
+$("#timeid").mouseleave(function(event){
+    isHovering = false;
+    console.log('I\m not hovering');
+});
 
 function displayTime() {
     d = new Date();
@@ -8,25 +20,61 @@ function displayTime() {
     minute = d.getMinutes();
     second = d.getSeconds();
 
-    if(hour <= 9) hour = '0'+hour;
-    if(minute <= 9) minute = '0'+minute;
-    if(second <= 9) second = '0'+second;
+    if (hour <= 9) hour = '0' + hour;
+    if (minute <= 9) minute = '0' + minute;
+    if (second <= 9) second = '0' + second;
 
-   document.querySelector('.time').textContent= hour + ":" + minute + ":" +second
-   x = second;
-   per = 1.667;
+    var hexHour = ('0' + hour.toString(16)).slice(-2);
+    var hexMinute = ('0' + minute.toString(16)).slice(-2);
+    var hexSecond = ('0' + second.toString(16)).slice(-2);
 
-   result = x * per;
-   percent = result + "%";
-   console.log(percent);
+    if(isHovering){
+        // if true do this
+        document.querySelector('.time').textContent = '#' + hexHour + hexMinute + hexSecond;
 
-   currentTime = hour + ':' + minute + ':' + second;
-   color = "#" + hour + minute + second;
+    } else {
+        // else do this
+        document.querySelector('.time').textContent = hour + ":" + minute + ":" + second;
+    }
 
-   document.body.style.background = color;
 
 
+    x = second;
+    per = 1.667;
+
+    result = x * per;
+    percent = result + "%";
+    // console.log(percent);
+
+
+
+    var color = "#" + hexSecond + hexMinute + hexSecond;
+
+    console.log('color', color);
+
+
+
+
+    document.getElementById("timeid").style.backgroundColor = color;
 }
 
-
 var timer = setInterval(displayTime, 1000);
+
+
+var isHovering = false;
+
+$("#timeid").mouseenter(function(event){
+    isHovering = true;
+    console.log('I\'m hovering');
+});
+
+$("#timeid").mouseleave(function(event){
+    isHovering = false;
+    console.log('I\m not hovering');
+});
+
+
+
+
+
+
